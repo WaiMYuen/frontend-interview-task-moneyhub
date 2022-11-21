4dab818f2fca43476158a2edfe09e903005bca6d
WaiMYuen <waiwaiming32@hotmail.com>
2022-11-21 19:23:48 +0000

    Fetch account data server-side

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Detail from "../modules/property-details";
 import {Banner} from "../components/banner"
 
-export default function PropertyDetails() {
+export default function PropertyDetails (props) {
   return (
     <>
       <Banner>Property Details</Banner>
-      <Detail/>
+      <Detail account={props}/>
     </>   
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/api/account')
+
+  const data = await response.json()
+
+  return {
+    props: data.account
+  }
 }
